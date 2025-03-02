a7c5d8fbfd317eb20ddf1e8134ed5347a1de3153
alimuhammed_Dev <muhammedaliali2020@gmail.com>
2025-03-02 06:14:53 +0100

    commit

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -11,9 +11,9 @@ const Home = () => {
 
       <div className="relative p-4 lg:mt-36 md:mt-28 mt-16">
         <img src={hero} alt="" className="w-full" />
-        <div className="absolute lg:top-36 top-5 lg:right-36 space-y-2 right-10">
+        <div className="absolute lg:top-36 top-5 lg:right-36 md:right-20 space-y-2 right-10">
           <h1 className="text-white text-xl lg:text-5xl">Enjoy up to 50% off <br /> on selected items</h1>
-          <p className="lg:block hidden text-white text-3xl">Shop on all your favourite items on Mashop today, <br /> We are the best e-commerce brand and cutomers <br /> satisfaction is our top priority</p>
+          <p className="lg:block md:block hidden text-white text-3xl">Shop on all your favourite items on Mashop today, <br /> We are the best e-commerce brand and cutomers <br /> satisfaction is our top priority</p>
           <div>
             <button className="bg-black text-white py-2 px-2 rounded-md">
               Shop Now
