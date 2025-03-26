4f2e8c50056fb459d496266facc2f2bddf4bf8c6
alimuhammed_Dev <muhammedaliali2020@gmail.com>
2025-03-26 15:51:07 +0100

    finished on product page

--- src/Components/Review.jsx
+++ src/Components/Review.jsx
@@ -10,6 +10,15 @@ const Review = () => {
       <div className="p-4 lg:mt-36 md:mt-36 mt-16">
         <GobackButton />
       </div>
+
+      <div className="p-4 space-y-3">
+        <h1 className="text-black text-xl font-semibold">
+          Product Ratings and Review
+        </h1>
+        <p className="text-gray-500">
+          You can only review this product after purchase.
+        </p>
+      </div>
     </section>
   );
 };
