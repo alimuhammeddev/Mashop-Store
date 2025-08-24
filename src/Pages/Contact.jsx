189460cab47fc5205dad265abe5d60023f4ce5c8
alimuhammed_Dev <muhammedaliali2020@gmail.com>
2025-08-24 14:31:20 +0100

    commit

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -8,14 +8,18 @@ const Contact = () => {
             <Header />
         </div>
 
-        <div className='p-4 lg:mt-36 mt-16'>
+        <div className='lg:p-0 p-4 lg:mt-36 mt-16 lg:max-w-7xl mx-auto'>
             <div>
                 <h1 className='text-center lg:text-3xl text-xl font-medium'>Contact us</h1>
             </div>
 
-            <div className='grid lg:grid-cols-2 grid-cols-1 gap-8 mt-8'>
+            <div className='grid lg:grid-cols-2 grid-cols-1 gap-8 lg:mt-8 mt-4'>
                 <div>
                     <img src={contact} alt="" />
+                </div>
+
+                <div>
+                    
                 </div>
             </div>
         </div>
